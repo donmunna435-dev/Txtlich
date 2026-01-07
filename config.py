d89343fc2a24475b478b56a4e071a7efcc94078a
donmunna435-dev <donmunna435@gmail.com>
2026-01-07 21:11:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7929130193:AAFJswIMtSV_CPazyAPKW4BQ58ijov3t-5I")
+    API_ID = int(os.environ["API_ID", 28094744]
+    API_HASH = os.environ["API_HASH", "a75af4285edc7747c57bb19147ca0b9b"]
+    AUTH_USERS = "5680454765"""
+ 
